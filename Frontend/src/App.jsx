ac0d7ba3785c99bb71df46a6ba383fb0d216dc43
Vladimir Kravtsov <you@example.com>
2026-05-05 21:31:21 +0300

    Added three empty pages with simple nav

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Board } from './pages/Board';
+import { Login } from './pages/Login';
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/login">Login</Link> |{" "}
+        <Link to="/board">Board</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/board" element={<Board />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
