a2c5380619cb983c9c2b70126be8ecc646c80ad0
Kirill <kiril.muravjev@gmail.com>
2026-05-15 16:57:35 +0300

    Added JSON serializer ignoring

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
 
 builder.Services.AddEndpointsApiExplorer();
