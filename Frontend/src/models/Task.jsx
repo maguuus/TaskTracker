6f924fd6f2088ec68c5a851861e91798ff2a3664
Margaritca2008 <muza20150109@gmail.com>
2026-05-16 15:17:08 +0300

    task-with-bootstrap

--- Frontend/src/models/Task.jsx
+++ Frontend/src/models/Task.jsx
@@ -1,23 +1,32 @@
 import { Card, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Task({ title, description, ...rest }) {
     return (
-        <Card className="mb-3 shadow-sm">
-            <Card.Body>
-                <Card.Title className="h6">{title}</Card.Title>
-                <Card.Text className="small text-muted">
-                    {description}
-                </Card.Text>
 
+        <Card className="border-primary shadow-sm p-3 mb-5 bg-white rounded ">
+            <Card.Body>
+                <h5> {description} </h5>
+                <button 
+                    className="btn btn-success rounded-pill position-absolute top-2 start-2"
+                    style={{
+                        top: '8px',
+                        left: '8px',
+                        fontSize: '0.65rem',
+                        padding: '2px 8px'}}
+                >
+                    {"#работа"}
+                </button>
                 {/* Кнопки */}
-                <div className="d-flex gap-2 mt-2">
-                    <Button variant="outline-primary" size="sm">
+                <div className="d-flex gap-3 mt-4 justify-content-end">
+                    <button 
+                        className="btn-close position-absolute top-0 end-0 mt-3 me-4"
+                        aria-label="Close"
+                    ></button>
+                    <Button variant="info d-inline-flex focus-ring focus-ring-info py-1 px-2 text-decoration-none border rounded-2" size="sm">
                         Редактировать
                     </Button>
-                    <Button variant="outline-danger" size="sm">
-                        Удалить
-                    </Button>
-                    <Button variant="outline-secondary" size="sm">
+                    <Button variant="secondary  d-inline-flex focus-ring focus-ring-secondary py-1 px-2 text-decoration-none border rounded-2" size="sm">
                         Детали
                     </Button>
                 </div>
