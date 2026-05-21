7040f82991718a4045636051007c2e03f8a1cec3
TimaK <tim07mes@mail.ru>
2026-05-21 15:09:03 +0300

    Добавлен запуск миграций, просто без комментариев...

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -37,6 +37,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
